0a636307773d44a03c3f5a76dfc62cd16f1d555f
AnthonyScott <anthony.scott1005@gmail.com>
2024-02-12 17:52:19 -0500

    Update powerset generator

--- subsetCreator/A.py
+++ subsetCreator/A.py
@@ -1,16 +1,22 @@
 def powerset(arr):
     """
-    Returns every possible subset from the given array.
+    Returns every possible subset from a given array
     """
-    n = len(arr)
-    subsets = []
-    for i in range(2**n):
-        subset = []
-        for j in range(n):
-            if (i & (1 << j)) != 0:
-                subset.append(arr[j])
-        subsets.append(subset)
-    return subsets
+    def backtrack(arr, subset, index):
+        if index == len(arr):
+            # Reached the end of the array, add the current subset to the result
+            result.append(subset[:])
+        else:
+            # Try including the current element in the subset
+            subset.append(arr[index])
+            backtrack(arr, subset, index + 1)
+            # Try excluding the current element from the subset
+            subset.pop()
+            backtrack(arr, subset, index + 1)
 
-arr = [1, 2, 3,4]
-print(powerset(arr))
+    result = []
+    backtrack(arr, [], 0)
+    return result
+
+arr = [1, 2, 3, 4]
+print(powerset(arr)) 
