987c5dc6a86b5eb3fb43682e004d94d583fbbe6c
AnthonyScott <anthony.scott1005@gmail.com>
2024-03-20 13:46:28 -0400

    Add sorting and filtering to the cake manager

--- cake-manager/a.py
+++ cake-manager/a.py
@@ -19,17 +19,28 @@ class CakeList:
     def check_expired(self):
         self.cakes = [cake for cake in self.cakes if cake.days_until_expire > 0]
 
+    def filter_by_ingredients(self, ingredient):
+        return [cake for cake in self.cakes if ingredient in cake.ingredients]
+
+    def sort_by_expiration_days(self):
+        self.cakes.sort(key=lambda cake: cake.days_until_expire)
+
 # Demonstration
 cake1 = Cake("Chocolate", 5, ["flour", "sugar", "chocolate"], "delicious", 2)
 cake2 = Cake("Vanilla", 3, ["flour", "sugar", "milk"], "yummy", 0)
+cake3 = Cake("Carrot", 4, ["flour", "sugar", "carrots"], "nice", 1)
 
 cake_list = CakeList()
 cake_list.add_cake(cake1)
 cake_list.add_cake(cake2)
+cake_list.add_cake(cake3)
 
-print("Cakes before check:", [cake.name for cake in cake_list.cakes])
-cake_list.check_expired()
-print("Cakes after check:", [cake.name for cake in cake_list.cakes])
+print("Cakes with flour:")
+for cake in cake_list.filter_by_ingredients("milk"):
+    print(cake.name)
 
-cake_list.delete_cake("Chocolate")
-print("Cakes after deleting Chocolate:", [cake.name for cake in cake_list.cakes])
+print("\nCakes sorted by expiration days:")
+cake_list.sort_by_expiration_days()
+for cake in cake_list.cakes:
+    print(cake.name, cake.days_until_expire)
+
--- cake-manager/b.py
+++ cake-manager/b.py
@@ -6,7 +6,7 @@ class Cake:
         self.flavor = flavor
         self.days_until_expire = days_until_expire
 
-class CakeManager:
+class CakeList:
     def __init__(self):
         self.cakes = []
 
@@ -16,23 +16,31 @@ class CakeManager:
     def delete_cake(self, name):
         self.cakes = [cake for cake in self.cakes if cake.name != name]
 
-    def delete_expired_cakes(self):
+    def check_expired(self):
         self.cakes = [cake for cake in self.cakes if cake.days_until_expire > 0]
 
+    def filter_by_ingredients(self, ingredient):
+        return [cake for cake in self.cakes if ingredient in cake.ingredients]
+
+    def sort_by_expiration_days(self):
+        self.cakes.sort(key=lambda cake: cake.days_until_expire)
+
 # Demonstration
-cake1 = Cake("Chocolate", 5, ["flour", "sugar", "chocolate"], "chocolate", 5)
-cake2 = Cake("Vanilla", 3, ["flour", "sugar", "milk"], "vanilla", 0)
+cake1 = Cake("Chocolate", 5, ["flour", "sugar", "chocolate"], "delicious", 2)
+cake2 = Cake("Vanilla", 3, ["flour", "sugar", "milk"], "yummy", 0)
+cake3 = Cake("Carrot", 4, ["flour", "sugar", "carrots"], "nice", 1)
 
-manager = CakeManager()
-manager.add_cake(cake1)
-manager.add_cake(cake2)
+cake_list = CakeList()
+cake_list.add_cake(cake1)
+cake_list.add_cake(cake2)
+cake_list.add_cake(cake3)
 
-print("Cakes before deletion:")
-for cake in manager.cakes:
+# Filter by ingredients
+filtered_cakes = cake_list.filter_by_ingredients("chocolate")
+for cake in filtered_cakes:
     print(cake.name)
 
-manager.delete_expired_cakes()
-
-print("\nCakes after deletion:")
-for cake in manager.cakes:
-    print(cake.name)
+# Sort by expiration days
+cake_list.sort_by_expiration_days()
+for cake in cake_list.cakes:
+    print(cake.name, cake.days_until_expire)
