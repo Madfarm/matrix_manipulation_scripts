7fcfb06566471252f69ec82c80db74d8e58f144c
AnthonyScott <anthony.scott1005@gmail.com>
2024-04-09 15:40:07 -0400

    Add processing function for legal drinkers

---
+++ model-generated/can-drink-calc/a.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+def check_age(df):
+    if 'Age' in df.columns:
+        df['can_drink'] = df['Age'] >= 21
+        return df
+    else:
+        return df
+
+
+df = pd.DataFrame({'Name': ['Seth', 'Esha', 'Jiametryc', 'Ash'],
+                   'GPA': [2.4, 3.8, np.nan, np.nan], 
+                   'Age': [30, 56, 8, np.nan],
+                   'Salary': [np.nan, 80000, 41000, 33000],
+                })
+
+
+print(check_age(df))
